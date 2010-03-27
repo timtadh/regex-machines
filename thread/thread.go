286f4b1f65c9aea80ba3d3618021be53ae88a27d
Tim Henderson <tim.tadh@gmail.com>
2010-03-27 18:00:31 -0400

    thread package added for backtracking vm
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ thread/thread.go
@@ -0,0 +1,38 @@
+package thread
+
+import "container/list"
+
+type Thread struct {
+    Pc uint32
+    Tc uint32
+}
+
+func NewThread(pc, tc uint32) *Thread {
+    self := new(Thread)
+    self.Pc = pc
+    self.Tc = tc
+    return self
+}
+
+type Stack struct {
+    list *list.List
+}
+
+func NewStack() *Stack {
+    self := new(Stack)
+    self.list = list.New()
+    return self
+}
+
+func (self *Stack) Empty() bool { return self.list.Len() <= 0 }
+
+func (self *Stack) Push(t *Thread) {
+    self.list.PushBack(t)
+}
+
+func (self *Stack) Pop() *Thread {
+    e := self.list.Back()
+    t, _ := e.Value.(*Thread)
+    self.list.Remove(e)
+    return t
+}
